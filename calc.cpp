2c0955fef48ae2bd386836f251c48bc007ac5943
Nikita7181 <70635031+Nikita7181@users.noreply.github.com>
2021-01-11 00:56:25 +0300

    Update calc.cpp

--- calc.cpp
+++ calc.cpp
@@ -11,34 +11,34 @@ int main ()
     
     std::string input;
     std::getline(std::cin, input);
+
     
-    
-    std::string expression;
-    std::cout << expression << std::endl;
-    
-    
+    //std::string expression;
+    std::cout << input << std::endl; 
+
+
     std::vector<char*> strStack_;
-    
-    input = "";
-    
+
+    //input = "";
+
     std::vector<double> stack_;
     double whole = 0;
     double fraction = 0;
-    double PI = 3.1415;
+    double PI = 3.14159265358979323846264338327950288419716939937510582097494459230781640628620899862803482534211706798214808651328230664709384460955058223172;
     int pow = 1;
     bool flag = true;
     bool flag_pow = false;
-    
-    
+
+
     int inputSize = input.size();
-    
+
     for (int i = 0; i < inputSize; i++)
     {
         char c = input[i];
         if (isdigit(c))
         {
             if (!flag_pow)
-            {
+            {  
                 whole *= 10;
                 whole += (c - '0');
                 flag = true;
@@ -47,58 +47,184 @@ int main ()
             {
                 fraction *= 10;
                 fraction += (c - '0');
-                flag = true;
-                pow++;
+                flag = true;          
+                pow++; 
             }
+            
+        }
+        else if (isalpha (c))
+        {
+            if ((c == 'P') && (i < inputSize) && (input[i+1] == 'I') )
+            {
+                stack_.push_back(PI);
+                flag = false;
+                i++;
+            }
+            else if ((c == 'S') && (i < inputSize - 1) && (input[i+1] == 'I') && (input[i+2] == 'N') )
+            {
+                double num = stack_.back();
+                stack_.pop_back();
+                double tmp = std::sin(num);
+                stack_.push_back(tmp);
+
+                flag = false;
+                //flag_pow = false;
+            }
+            else if ((c == 'C') && (i < inputSize - 1) && (input[i+1] == 'O') && (input[i+2] == 'S') )
+            {
+                double num = stack_.back();
+                stack_.pop_back();
+                double tmp = std::cos(num);
+                stack_.push_back(tmp);
+
+                flag = false;
+                //flag_pow = false;
+            }
+            else if ((c == 'T') && (i < inputSize - 1) && (input[i+1] == 'A') && (input[i+2] == 'N') )
+            {
+                double num = stack_.back();
+                stack_.pop_back();
+                double tmp = std::tan(num);
+                stack_.push_back(tmp);
+
+                flag = false;
+                //flag_pow = false;
+            }
+            else if ((c == 'C') && (i < inputSize - 1) && (input[i+1] == 'T') && (input[i+2] == 'G') )
+            {
+                double num = stack_.back();
+                stack_.pop_back();
+                if (std::tan(num) == 0) 
+                {
+                    std::cout << "NAN" << std::endl;
+                     return 0;
+                }
+                else
+                {
+                    double tmp = 1 / std::tan(num);
+                    stack_.push_back(tmp);
+
+                    flag = false;
+                }
+                //flag_pow = false;
+            }
+            else if ((c == 'E') && (i < inputSize - 1) && (input[i+1] == 'X') && (input[i+2] == 'P') )
+            {
+                double num = stack_.back();
+                stack_.pop_back();
+
+                double tmp = std::exp(num);
+                stack_.push_back(tmp);
+
+                flag = false;
+
+            }
+            else if ((c == 'S') && (i < inputSize - 2) && (input[i+1] == 'Q') && (input[i+2] == 'R') && (input[i+3] == 'T') )
+            {
+                double num = stack_.back();
+                stack_.pop_back();
+
+                double tmp = std::sqrt(num);
+                stack_.push_back(tmp);
+
+                flag = false;
+
+            }
+            else if ((c == 'P') && (i < inputSize - 1) && (input[i+1] == 'O') && (input[i+2] == 'W') )
+            {
+                double num2 = stack_.back();
+                stack_.pop_back();
+                double num1 = stack_.back();
+                stack_.pop_back();
+
+                double tmp = std::pow(num1, num2);
+                stack_.push_back(tmp);
+
+                flag = false;
+
+            }
+
             
         }
         else if ((c == '.' && flag) || (c == ',' && flag))
         {
             flag_pow = true;
         }
-        else if ((c == 'P') && (i < inputSize) && (input[i+1] == 'I') )
-        {
-            stack_.push_back(PI);
-            flag = false;
-            i++;
-        }
         else
         {
             if (c != ' ')
             {
-                double num2 = stack_.back();
-                stack_.pop_back();
-                double num1 = stack_.back();
-                stack_.pop_back();
-                
+
+
                 double tmp = 0;
-                switch (c){
-                    
+                switch (c)
+                {
+
                     case '+' :
-                        tmp = num1 + num2;
-                        stack_.push_back(tmp);
-                        break;
+                        {                       
+                            double num2 = stack_.back();
+                            stack_.pop_back();
+                            double num1 = stack_.back();
+                            stack_.pop_back();
+                            tmp = num1 + num2;
+                            stack_.push_back(tmp);
+                            break;
+                        }
                     case '-' :
-                        tmp = num1 - num2;
-                        stack_.push_back(tmp);
+                        {
+                            if ((i < inputSize) && (input[i+1] == '-'))
+                            {
+                                double num1 = stack_.back();
+                                stack_.pop_back();
+                                tmp = num1 - 1;
+                                i++;
+                            }
+                            else
+                            {
+                                double num2 = stack_.back();
+                                stack_.pop_back();
+                                double num1 = stack_.back();
+                                stack_.pop_back();
+                                tmp = num1 - num2;
+                            }
+                            stack_.push_back(tmp);
                         break;
+                        }
                     case '*' :
+                    {
+                        double num2 = stack_.back();
+                        stack_.pop_back();
+                        double num1 = stack_.back();
+                        stack_.pop_back();
                         tmp = num1 * num2;
                         stack_.push_back(tmp);
                         break;
+                    }
                     case '/' :
+                    {
+                        double num2 = stack_.back();
+                        stack_.pop_back();
+                        double num1 = stack_.back();
+                        stack_.pop_back();
                         tmp = num1 / num2;
                         stack_.push_back(tmp);
                         break;
+                    }
                     case '^' :
+                    {
+                        double num2 = stack_.back();
+                        stack_.pop_back();
+                        double num1 = stack_.back();
+                        stack_.pop_back();
                         tmp = std::pow(num1, num2);
                         stack_.push_back(tmp);
                         break;
+                    }
                     default:
                         break;
-                    
+
                 }
-                
+
                 flag = false;
                 flag_pow = false;
             }
@@ -114,11 +240,11 @@ int main ()
             }
         }
     }
-    
-    
+
+
     double result = stack_.size() > 0 ? stack_.back() : 0;
     int iResult = result;
-    if (result - iResult > 0)
+    if (result - iResult != 0) 
     {
         std::cout << std::fixed << std::setw(10) << result << std::endl;
     }
@@ -127,7 +253,7 @@ int main ()
         std::cout << std::fixed << std::setw(10) << std::setprecision(0) << result << std::endl;
     }
     
-    
+
     return 0;
     
 }
